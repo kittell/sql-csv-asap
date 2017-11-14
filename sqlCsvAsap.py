af648da13ed037f8898b6469f171395a921df9e1
Kirk Kittell <kirk.kittell@gmail.com>
2017-11-14 15:06:55 -0600

    removed camel case for functions and variables

--- sqlCsvAsap.py
+++ sqlCsvAsap.py
@@ -4,58 +4,58 @@ import os
 
 # METHODS
 
-def getUserQuery():
+def get_user_query():
     print('\nEnter SELECT-FROM-WHERE query:')
     valid = False
     while valid == False:
-        queryInput = input('query > ')
-        # parse queryInput into SELECT, FROM, WHERE, etc.
+        query_input = input('query > ')
+        # parse query_input into SELECT, FROM, WHERE, etc.
         # validate components
         valid = True #temporary to get out of validation loop
 
-    #userQuery = query(inputSelect, inputFrom, inputWhere)
-    userQuery = queryInput #temporary
-    return userQuery
+    #user_query = query(inputSelect, inputFrom, inputWhere)
+    user_query = query_input #temporary
+    return user_query
 
-def validateUserCommand(userCommand):
+def validate_user_command(user_command):
     # TODO - check if it's in the cmd dictionary
     # TODO - maybe also handle the capitalization
     valid = True
     return valid
 
-def executeUserCommand(userCommand):
+def execute_user_command(user_command):
     cmd = {
-        'help':cmdHelp,
-        'quit':cmdQuit,
-        'query':cmdQuery,
-        'show tables':cmdShowTables,
-        'show attributes in':cmdShowAttributes
+        'help':cmd_help,
+        'quit':cmd_quit,
+        'query':cmd_query,
+        'show tables':cmd_show_tables,
+        'show attributes in':cmd_show_attributes
     }
     #TODO: something to handle an invalid command
     
-    if 'show attributes in' in userCommand:
+    if 'show attributes in' in user_command:
         # get remainder of string after 'show attributes in'
         # TODO: for now, assume one space after 'show attributes in'; not nec. true
-        tableName = userCommand[len('show attributes in')+1:len(userCommand)]
-        userCommand = 'show attributes in'
-        return cmd[userCommand](tableName)
+        table_name = user_command[len('show attributes in')+1:len(user_command)]
+        user_command = 'show attributes in'
+        return cmd[user_command](table_name)
     else:
-        return cmd[userCommand]()
+        return cmd[user_command]()
 
-def getUserCommand():
+def get_user_command():
     """
     Retrieve a command line command from user.
     Validate it, then pass it back.
     """
     valid = False
     while valid == False:
-        userCommand = input('> ')
-        valid = validateUserCommand(userCommand)
-    return userCommand
+        user_command = input('> ')
+        valid = validate_user_command(user_command)
+    return user_command
 
-def getCsvList():
-    currentDirectory = os.getcwd()
-    tableDirectory = currentDirectory + '\\tables'
+def get_csv_list():
+    current_directory = os.getcwd()
+    table_directory = current_directory + '\\tables'
 
     # I don't think this is the best way to do this, but it works for now...
     # os.walk through /tables directory and add files to filenames list,
@@ -64,7 +64,7 @@ def getCsvList():
     
     # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory#3207973
     f = []
-    for (dirpath, dirnames, filenames) in os.walk(tableDirectory):
+    for (dirpath, dirnames, filenames) in os.walk(table_directory):
         f.extend(filenames)
         break
 
@@ -76,44 +76,44 @@ def getCsvList():
 
 # COMMAND METHODS
 
-def cmdHelp():
+def cmd_help():
     # Show list of commands, and how to use them
     print('TODO: description of available functions and how to use them')
     return True
 
-def cmdQuit():
+def cmd_quit():
     # Do nothing. Return False. This will cause the main loop to exit.
     print('Goodbye.')
     return False
 
-def cmdQuery():
+def cmd_query():
     # This is the signal to the program to collect a query from the user.
-    userQuery = getUserQuery()
+    user_query = get_user_query()
 
     # TODO: after getting the query, parse it into its components (select, from, where, etc.)
     return True
 
-def cmdShowTables():
+def cmd_show_tables():
     # show list of .csv files in /tables, one per line
 
-    csvList = getCsvList()
+    csv_list = get_csv_list()
     print('List of available tables:')
     for entry in filenames:
-        # tableName is the filename minus .csv
+        # table_name is the filename minus .csv
         # For now, assuming that all entries end in .csv, although there should be a test for it
-        tableName = entry[0:len(entry)-4]
-        print('*', tableName)
+        table_name = entry[0:len(entry)-4]
+        print('*', table_name)
 
     return True
 
-def cmdShowAttributes(tableName):
-    #TODO: pull values from first line of tableName.csv
-    print('TODO: show attributes in', tableName)
+def cmd_show_attributes(table_name):
+    #TODO: pull values from first line of table_name.csv
+    print('TODO: show attributes in', table_name)
     return True
 
 # VALIDATION METHODS
 
-def validateSelect(input):
+def validate_select(input):
     # Conditions for validity:
     # - Attribute must exist
     valid = False
@@ -123,7 +123,7 @@ def validateSelect(input):
     
     return valid
 
-def validateFrom(input):
+def validate_from(input):
     # Conditions for validity:
     # - Table must exist
     valid = False
@@ -133,7 +133,7 @@ def validateFrom(input):
     
     return valid
 
-def validateWhere(input):
+def validate_where(input):
     # Conditions for validity:
     # - Type must be correct
     valid = False
@@ -142,12 +142,12 @@ def validateWhere(input):
     valid = True
     return valid
 
-def validateQuery(validSelect, validFrom, validWhere):
+def validate_query(valid_select, valid_from, valid_where):
     #TODO: should have some meaningful error messages
     valid = True
-    if validSelect == False: valid = False
-    if validFrom == False: valid = False
-    if validWhere == False: valid = False
+    if valid_select == False: valid = False
+    if valid_from == False: valid = False
+    if valid_where == False: valid = False
 
     return valid
 
@@ -158,7 +158,7 @@ if __name__ == '__main__':
 
     # Keep running this loop while the user is using the program.
     # Only the command 'quit' should return False, thus exiting the loop.
-    keepGoing = True
-    while keepGoing == True:
-        userCommand = getUserCommand()
-        keepGoing = executeUserCommand(userCommand)
+    keep_going = True
+    while keep_going == True:
+        user_command = get_user_command()
+        keep_going = execute_user_command(user_command)
