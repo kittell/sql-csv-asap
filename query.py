e66ef1aa25ffa7dbeda7edd4e14b825b30688724
Kirk Kittell <kirk.kittell@gmail.com>
2017-12-14 01:35:37 -0600

    keyword indexing works
    code is pretty rickety though, needs to be redone a bit

--- query.py
+++ query.py
@@ -3,6 +3,91 @@ import csv
 import os
 import os.path
 from parse import Query
+from index import *
+
+# TODO: With indexing, decide here if there is a relevant index for this table
+# value_constraints contains the item numbers of constraint in q.WHERE
+# So: we're interested in q.WHERE[c]['Subject'], which will hold a table.attr pair.
+# Search the available indexes to see if there are any for these table.attr.
+# For now, just find one and break. Will deal w/ multiple later, maybe.
+
+def value_constraint_has_index(q, table_name):
+    # Loop through value_constraints, find a matching table.attr index file
+    if table_name in q.value_constraints:
+        for c in q.value_constraints[table_name]:
+            ta = q.WHERE[c]['Subject']
+            ta_split = parse_table_attribute_pair(ta)
+            if table_name == ta_split[0]:
+                # now see if there's an index corresponding to an attribute
+                if exists_index_file_keyword(ta_split[0], ta_split[1]) == True:
+                    return True
+    
+    return False
+
+def join_constraint_has_index(q, table_name='', attr_name=''):
+    # Loop through value_constraints, find a matching table.attr index file
+    if exists_index_file_keyword(table_name, attr_name) == True:
+        return True
+    
+    return False
+
+def get_relevant_row_list(q, table_name, attr_value=''):
+    # TODO: Limitation: only can handle one condition from WHERE. Should consider the effects
+    #   of multiple conditions.
+    # TODO: What happens if this returns an empty list? Should there be a different signal
+    #   that is returned if the index value isn't found in the index?
+    relevant_row_list = []
+    if table_name in q.value_constraints:
+        for c in q.value_constraints[table_name]:
+            ta = q.WHERE[c]['Subject']
+            ta_split = parse_table_attribute_pair(ta)
+            if table_name == ta_split[0]:
+                # now see if there's an index corresponding to an attribute
+                if exists_index_file_keyword(ta_split[0], ta_split[1]) == True:
+                    index = read_index_file_keyword(table_name, ta_split[1])
+                    obj = q.WHERE[c]['Object']
+                    op = q.WHERE[c]['Verb']
+
+                    if op == '=' and obj in index:
+                        # Heuristic: = is the most common operator. Get right to the point on this one.
+                        for i in index[obj]:
+                            try:
+                                relevant_row_list.append(int(i))
+                            except:
+                                continue
+                            break
+                    else:
+                        # Handle all operators
+                        for attr_value in index:
+                            print(attr_value, op, obj)
+                            if eval_binary_comparison(attr_value, op, obj) == True:
+                                for i in index[attr_value]:
+                                    try:
+                                        relevant_row_list.append(int(i))
+                                    except:
+                                        continue
+    test_print('get_relevant_row_list / relevant_row_list:', relevant_row_list)
+    return relevant_row_list
+
+
+def get_relevant_row_list_join(q, table_name, attr_name, attr_value):
+    # TODO: Limitation: only can handle one condition from WHERE. Should consider the effects
+    #   of multiple conditions.
+    # TODO: What happens if this returns an empty list? Should there be a different signal
+    #   that is returned if the index value isn't found in the index?
+    relevant_row_list = []
+    
+    if exists_index_file_keyword(table_name, attr_name) == True:
+        index = read_index_file_keyword(table_name, attr_name)
+        
+        if attr_value in index:
+            for i in index[attr_value]:
+                try:
+                    relevant_row_list.append(int(i))
+                except:
+                    continue
+    
+    return relevant_row_list
 
 
 def perform_query(q):
@@ -42,33 +127,53 @@ def perform_query(q):
         #   * Walk through the temp_join files, using table1, row1 to get the relevant 
         #       information from the temp_filtered_table1.csv file, and the same with
         #       table2, row2. Merge the information together into a single line and print it.
-        
+
     for table1 in q.query_table_list:
+        
+        # Get index info for table1
+        has_index1 = value_constraint_has_index(q, table1)
+        relevant_row_list1 = get_relevant_row_list(q, table1)
+        remaining_row_list1 = relevant_row_list1
     
         if len(q.join_constraints) == 0:
             # CASE 1: NO JOINS
             # Just apply value_constraints to single table
+            # TODO: This could be stuffed down in test_value_constraints, but it was the first
+            #   part of the query code I got to work, and it's just here
+            
             csv1 = table_to_csv_fullpath(table1)
             with open(csv1, newline = '', encoding = 'utf-8') as f1:
                 test_print('perform_query / open(csv1)', csv1)
                 r1 = csv.reader(f1)
-                i_row1 = 1
                 next(r1)            # Assumption: first row is header. Skip it.
                 for row1 in r1:
-                    i_row1 = i_row1 + 1
-                    int_select_results = []
-                    
+                    # INDEXING: decide here whether to read or skip the row
+                    if has_index1 == True:
+                        if len(relevant_row_list1) == 0:
+                            continue
+                        if r1.line_num not in relevant_row_list1:
+                            continue
+                        else:
+                            # Remove index value after you've used it
+                            remaining_row_list1.remove(r1.line_num)
+
                     # Skip over blank rows
                     if ''.join(row1).strip() == '':
                         continue
-                    
+                        
+                    int_select_results = []
                     # test row1 against value_constraints[table1]
                     pass_value_constraints = test_value_constraints(table1, row1, q)
                     if pass_value_constraints == True:
-                        append_filtered_row(table1, row1, i_row1)
+                        append_filtered_row(table1, row1, r1.line_num)
                         int_select_results = project_row(int_select_results, row1, table1, q)
-                        print(int_select_results)
-            f1.closed
+                    if len(int_select_results) > 0:
+                        final_select_results.append(int_select_results)
+                        
+                    # Break out of scanning the table if you've exhausted index entries
+                    if has_index1 == True:
+                        if len(remaining_row_list1) == 0:
+                            break
 
         else:
             # CASE 2: JOINS
@@ -81,25 +186,37 @@ def perform_query(q):
                     with open(csv1, newline = '', encoding = 'utf-8') as f1:
                         test_print('perform_query / open(csv1)', csv1)
                         r1 = csv.reader(f1)
-                        i_row1 = 1
                         next(r1)            # Assumption: first row is header. Skip it.
                         for row1 in r1:
-                            i_row1 = i_row1 + 1
+                            # INDEXING: decide here whether to read or skip the row
+                            if has_index1 == True:
+                                if len(relevant_row_list1) == 0:
+                                    continue
+                                if r1.line_num not in relevant_row_list1:
+                                    continue
+                                else:
+                                    # Remove index value after you've used it
+                                    remaining_row_list1.remove(r1.line_num)
 
                             # Skip over blank rows
                             if ''.join(row1).strip() == '':
                                 continue
-
                             # test row1 against value_constraints[table1]
                             pass_value_constraints = test_value_constraints(table1, row1, q)
                             if pass_value_constraints == True:
-                                pass_join_constraints = test_join_constraints(table1, row1, i_row1, q)
+                                pass_join_constraints = test_join_constraints(table1, row1, r1.line_num, q)
                                 if pass_join_constraints == True:
-                                    append_filtered_row(table1, row1, i_row1)
-                    f1.closed
-    
+                                    append_filtered_row(table1, row1, r1.line_num)
+                            
+                            # Break out of scanning the table if you've exhausted index entries
+                            if has_index1 == True:
+                                if len(remaining_row_list1) == 0:
+                                    break
+                                
     # Further processing for case 2: gather results from temp files and combine them
+    # TODO: Split this out into a separate method
     for table_pair in q.join_constraints:
+        test_print('perform_query / rejoin:', table_pair)
         table1 = table_pair[0]
         table2 = table_pair[1]
         filtered_tables = {}
@@ -119,7 +236,7 @@ def perform_query(q):
                     int_select_results = []
                     for col in range(len(row_join)):
                         # Next: go to filtered_table files to retrieve data
-                        # Columns 0, 2, 4, etc. have table namess; 1, 3, etc. have row numbers (i_filtered)
+                        # Columns 0, 2, 4, etc. have table names; 1, 3, etc. have row numbers (i_filtered)
                         if col % 2 == 0:
                             if os.path.exists(filtered_tables[row_join[col]]):
                                 with open(filtered_tables[row_join[col]], newline = '', encoding = 'utf-8') as f_filtered:
@@ -132,13 +249,14 @@ def perform_query(q):
                                             # Note: leaving out first entry in row_filtered: it is
                                             # a row #, not part of results
                                             int_select_results = project_row(int_select_results, row_filtered[1:len(row_filtered)], table_filtered, q)
+                                            test_print('perform_query / int_select_results:', int_select_results)
+                                            
                                             break
                                 f_filtered.closed
-                    
-                    print(int_select_results)
-
+                    if len(int_select_results) > 0:
+                        final_select_results.append(int_select_results)
             f_join.closed
-    
+
     return final_select_results
 
 def project_row(results, row, table, q):
@@ -203,8 +321,8 @@ def test_value_constraints(table, row, q):
     return result
 
 def test_join_constraints(table1, row1, i_row1, q):
-    """TEST_WHERE_CONSTRAINTS
-        DESCRIPTION: 
+    """TEST_JOIN_CONSTRAINTS
+        DESCRIPTION: Test a data row from a csv file against a second csv file.
         INPUT:
             - row: list of parsed values from a row in a csv file
             - where_constraints
@@ -226,6 +344,7 @@ def test_join_constraints(table1, row1, i_row1, q):
     table2_list = []
     join_results = {}
     
+    # Determine which tables need to be opened and tested
     for table_pair in q.join_constraints:
         if table_pair[0] == table1:
             if table_pair[1] not in table2_list:
@@ -234,19 +353,49 @@ def test_join_constraints(table1, row1, i_row1, q):
     # 1 - Only need to do further processing if table1 shows up as the first in a pair of join_constraints
     #       (if it's the second, it will be handled later)
     # TODO: in query parsing, there should be some reordering of join constraints to limit re-reading files
+    #       i.e. optimization
     
     # loop through available combos of table2 (of a table1-table2 pair)
     # loop through the where constraints on that pair
     for table2 in table2_list:
+        
+        # TODO - this isn't ideal. Getting an attribute name and value from row1 to send along
+        #   to find relevant rows from index. Just picking the first one.
+        attr_name2 = ''
+        attr_value1 = ''
+        for table_pair in q.join_constraints:
+            if table_pair[0] == table1:
+                if table_pair[1] == table2:
+                    # Get attribute name from right side of join equation
+                    constraint_list = q.join_constraints[table_pair]
+                    i_where = constraint_list[0]
+                    ta1 = q.WHERE[i_where]['Subject']
+                    ta2 = q.WHERE[i_where]['Object']
+                    ta_split1 = parse_table_attribute_pair(ta1)
+                    ta_split2 = parse_table_attribute_pair(ta2)
+                    attr_name2 = ta_split2[1]
+                    
+        # INDEXING: Get index info for table2
+        has_index2 = join_constraint_has_index(q, table2, attr_name2)
+
+        attr_index1 = get_attribute_index(ta1, q.attribute_dict)
+        attr_value1 = row1[attr_index1]
+        relevant_row_list2 = get_relevant_row_list_join(q, table2, attr_name2, attr_value1)
+        
         # walk through table 2
         csv2 = table_to_csv_fullpath(table2)
         with open(csv2, newline = '', encoding = 'utf-8') as f2:
 #            test_print('test_join_constraints / open(csv2)', csv2)
-            i_row2 = 1
             r2 = csv.reader(f2)
             next(r2)
             for row2 in r2:
-                i_row2 = i_row2 + 1
+                # INDEXING: decide here whether to read or skip the row
+                if has_index2 == True:
+                    if r2.line_num not in relevant_row_list2:
+                        continue
+                    if len(relevant_row_list2) == 0:
+                        continue
+
                 # Skip over blank rows, it's a killer
                 if ''.join(row2).strip() == '':
                     continue
@@ -297,27 +446,24 @@ def test_join_constraints(table1, row1, i_row1, q):
 
                     if result == True:
                         persistent_result = True
-                        append_join_pair(table1, i_row1, table2, i_row2)
-                        append_filtered_row(table2, row2, i_row2)
+                        append_join_pair(table1, i_row1, table2, r2.line_num)
+                        append_filtered_row(table2, row2, r2.line_num)
 
         f2.closed
     
     return persistent_result
 
 def find_join_row(v1, a2, csv2):
-    #TODO: no return value at the moment, perhaps should send something back to say yes/no found something
     found = False
     
     # don't join null values
     if v1 != '':
         test_print('find_join_value / v1', v1)
         with open(csv2, newline = '', encoding = 'utf-8') as f2:
-            i_row2 = 1
             r2 = csv.reader(f2)
             next(r2)        # Assumption: first row is header. Skip it.
         
             for row2 in r2:
-                i_row2 = i_row2 + 1
                 # Skip over blank rows, it's a killer
                 if ''.join(row2).strip() == '':
                     continue
@@ -328,20 +474,14 @@ def find_join_row(v1, a2, csv2):
                 if v1 == v2:
                     test_print('perform_query / v2', v2)
                     found = True
-                    break
+                    return r2.line_num
         f2.closed
     
-    if found == False:
-        i_row2 = 0
-    
-    return i_row2
+    return 0
     
 
 def append_join_pair(table1, i_row1, table2, i_row2):
-    
-    filedir = get_table_directory()
-    filename = 'temp_join_' + table1 + '_' + table2 + '.csv'
-    filepath = os.path.join(filedir, filename)
+    filepath = get_temp_join_fullpath(table1, table2)
     
     writeline = [table1, i_row1, table2, i_row2]
     
